3c324774278e71ffc63c17bd9f662c3f1f1c32ce
ydmins <dr.ydmins@gmail.com>
2025-03-06 19:40:10 +0900

    feat : Implement count page with cubit state management

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
+import 'package:count_state_manage/src/cubit/count_cubit.dart';
+import 'package:count_state_manage/src/cubit/cubit_screen.dart';
 import 'package:count_state_manage/src/getx/count_getx_controller.dart';
 import 'package:count_state_manage/src/getx/getx_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 class App extends StatelessWidget {
@@ -29,7 +32,15 @@ class App extends StatelessWidget {
                   );
                 },
                 child: Text('Add with Getx')),
-            ElevatedButton(onPressed: () {}, child: Text('Add with Cubit')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.to(
+                      BlocProvider(
+                          create: (context) => CountCubit(),
+                          child: CubitScreen()),
+                      duration: Duration.zero);
+                },
+                child: Text('Add with Cubit')),
             ElevatedButton(onPressed: () {}, child: Text('Add with Bloc'))
           ],
         ),
---
+++ lib/src/cubit/cubit_screen.dart
@@ -0,0 +1,40 @@
+import 'package:count_state_manage/src/cubit/count_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CubitScreen extends StatelessWidget {
+  const CubitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Managment - Cubit'),
+      ),
+      body: Center(
+        child: BlocBuilder<CountCubit, int>(builder: (context, state) {
+          return Text(
+            state.toString(),
+            style: TextStyle(fontSize: 80),
+          );
+        }),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: context.read<CountCubit>().addCount,
+            backgroundColor: Colors.green,
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 30),
+          FloatingActionButton(
+            onPressed: context.read<CountCubit>().subtractCount,
+            backgroundColor: Colors.red,
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
